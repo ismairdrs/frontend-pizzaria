d677967a90191036202f792faf2081f25e1c4261
ismairdrs <ismairdrs@outlook.com>
2021-04-21 12:08:19 -0300

    fix

--- server.js
+++ server.js
@@ -1,17 +1,18 @@
 const express = require('express');
 const app = express();
 
-const PORT = process.env.PORT || 8080;
 app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*"); 
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
   });
+
+const PORT = process.env.PORT || 8080;
+
 app.use(express.static(__dirname + '/dist/frontend'));
 
-app.get('/*', cors(corsOptions), (req, res) => {
+app.get('/*', (req, res) => {
     res.sendFile(__dirname + '/dist/frontend/index.html');
-    res.json({msg: 'cors'})
 });
 
 app.listen(PORT, () => {

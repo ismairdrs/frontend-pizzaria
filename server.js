b2e08b222a321d52672fa7c936703a219b04575d
ismairdrs <ismairdrs@outlook.com>
2021-04-21 12:10:40 -0300

    fix

--- server.js
+++ server.js
@@ -1,16 +1,16 @@
 const express = require('express');
 const app = express();
 
+
+
+const PORT = process.env.PORT || 8080;
+
+app.use(express.static(__dirname + '/dist/frontend'));
 app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*"); 
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
   });
-
-const PORT = process.env.PORT || 8080;
-
-app.use(express.static(__dirname + '/dist/frontend'));
-
 app.get('/*', (req, res) => {
     res.sendFile(__dirname + '/dist/frontend/index.html');
 });

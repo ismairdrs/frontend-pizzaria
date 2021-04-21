75b82ecfb7bcc81fe6900a80ab69eb65944aa09a
ismairdrs <ismairdrs@outlook.com>
2021-04-21 11:54:17 -0300

    fix

--- server.js
+++ server.js
@@ -1,18 +1,22 @@
 const express = require('express');
+const cors = require('cors');
 const app = express();
 
-app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    next();
-  });
+var corsOptions = {
+    origin: function (origin, callback) {
+      db.loadOrigins(function (error, origins) {
+        callback(error, origins)
+      })
+    }
+  }
 
 const PORT = process.env.PORT || 8080;
 
 app.use(express.static(__dirname + '/dist/frontend'));
 
-app.get('/*', (req, res) => {
+app.get('/*', cors(corsOptions), (req, res) => {
     res.sendFile(__dirname + '/dist/frontend/index.html');
+    res.json({msg: 'cors'})
 });
 
 app.listen(PORT, () => {
